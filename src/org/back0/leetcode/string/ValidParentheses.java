cfd6ed45d1b76ae53ae64638a9651741c9e7cc46
ck0ba <sbackovic93@gmail.com>
2024-04-27 16:10:12 +0200

    Solve "Valid Parentheses" problem

---
+++ src/org/back0/leetcode/string/ValidParentheses.java
@@ -0,0 +1,27 @@
+package org.back0.leetcode.string;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        var closingParentheses = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char currChar = s.charAt(i);
+            if (currChar == '(') {
+                closingParentheses.push(')');
+            } else if (currChar == '[') {
+                closingParentheses.push(']');
+            } else if (currChar == '{') {
+                closingParentheses.push('}');
+            } else if (closingParentheses.empty() || closingParentheses.pop() != currChar) {
+                return false;
+            }
+        }
+
+        return closingParentheses.empty();
+    }
+
+}
