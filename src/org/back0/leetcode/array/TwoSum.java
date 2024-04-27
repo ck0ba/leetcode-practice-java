9230c26c7c5158cfcda48ca2f8aa66d5f5f4681a
ck0ba <sbackovic93@gmail.com>
2024-04-27 15:35:50 +0200

    Solve "Two Sum" problem

--- src/org/back0/leetcode/array/TwoSum.java
+++ src/org/back0/leetcode/array/TwoSum.java
@@ -1,2 +1,26 @@
-package org.back0.leetcode.array;public class TwoSum {
+package org.back0.leetcode.array;
+
+import java.util.HashMap;
+
+//https://leetcode.com/problems/two-sum/description/
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        var numToIndex = new HashMap<Integer, Integer>();
+        var res = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (numToIndex.containsKey(target - nums[i])) {
+                res[0] = numToIndex.get(target - nums[i]);
+                res[1] = i;
+
+                break;
+            }
+
+            numToIndex.put(nums[i], i);
+        }
+
+        return res;
+    }
+
 }
