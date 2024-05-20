40ef5d8fda8ddd4581f274835f6f812297537258
ck0ba <sbackovic93@gmail.com>
2024-05-20 12:28:35 +0200

    Solve 'StacksAndQueues' problem

---
+++ src/org/back0/leetcode/stack_and_queue/StacksAndQueues.java
@@ -0,0 +1,41 @@
+package org.back0.leetcode.stack_and_queue;
+
+import java.util.Stack;
+
+public class StacksAndQueues {
+    private final Stack<Integer> enqueueStack;
+    private final Stack<Integer> dequeueStack;
+
+    public StacksAndQueues() {
+        enqueueStack = new Stack<>();
+        dequeueStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        enqueueStack.push(x);
+    }
+
+    public int pop() {
+        ensureOrderForPop();
+
+        return dequeueStack.pop();
+    }
+
+    public int peek() {
+        ensureOrderForPop();
+
+        return dequeueStack.peek();
+    }
+
+    public boolean empty() {
+        return enqueueStack.empty() && dequeueStack.empty();
+    }
+
+    private void ensureOrderForPop() {
+        if (dequeueStack.empty()) {
+            while (!enqueueStack.empty()) {
+                dequeueStack.push(enqueueStack.pop());
+            }
+        }
+    }
+}
